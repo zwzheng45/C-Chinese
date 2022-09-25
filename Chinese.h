126136796ae8577b755800d2ceadaad4beddeb71
ZZW <62459108+zwzheng45@users.noreply.github.com>
2022-09-25 23:56:49 +0100

    上传头文件

---
+++ Chinese.h
@@ -0,0 +1,52 @@
+//
+//  Chinese.h
+//  ZZWC
+//
+//  Created by ZZW on 2022/9/25.
+//
+
+#ifndef Chinese_h
+#define Chinese_h
+
+
+#define 整 int
+#define 字 char
+#define 分 float
+#define 無符 unsigned
+#define 空 void
+#define 時類 time_t
+#define 退 return
+#define 返(文) return(文)
+#define 止(文) exit(文)
+
+#define 若 if
+#define 然則 else
+#define 環 for
+#define 若環 while
+
+#define 隨源(種) srand(種)
+
+#define 印 printf
+#define 輸入 scanf
+#define 獲字 getchar()
+#define 錯(文) perror(文)
+#define 遷(新文,舊文) strcpy(新文,舊文)
+#define 比文(文,另文) strcmp(文,另文)
+
+#define 刷緩 fflush(stdin)
+#define 獲時(號) time(號)
+#define 時差(壹號,貳號) difftime(壹號,貳號)
+#define 為字(文) isalpha(文)
+#define 為數(文) isdigit(文)
+#define 量(文) sizeof(文)
+
+#define 成數(文) atoi(文)
+#define 讀文 fscanf
+#define 寫文 fprintf
+#define 文啟 fopen
+#define 文關(文) fclose(文)
+#define 文末(文) feof(文)
+#define 虛無 NULL
+
+
+#endif /* Chinese_h */
